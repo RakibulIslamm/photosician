a1368314b834f16581ff16584a698ff74d3e6eac
RakibulIslam <rakibulislamsofficial@gmail.com>
2022-04-17 02:01:45 +0600

    initial commit

---
+++ data.js
@@ -0,0 +1,34 @@
+const services = [
+    {
+        id: 1,
+        title: 'Event Photography',
+        shortDesc: 'food festivals, dinner evenings, product launches and pop-ups',
+        desc: 'Event photographer Jamie Lau photographs various events, capturing the atmosphere and personalities of each occasion. From conference photography for Marks and Spencer, product launches, seminars to restaurant Pop-Ups and opening nights for bars throughout London, Oxford and Brighton. ',
+        img: 'https://i.ibb.co/v3BQWSZ/Photo-Album-2-icon.png',
+        price: 250
+    },
+    {
+        id: 2,
+        title: 'Food & Drink Photography',
+        shortDesc: 'restaurants, food, drinks, interiors and portraits',
+        desc: 'Accomplished at working with restaurants, publishers and PR & Advertising agencies to create distinctive Food & Drink photography. Including studio photoshoots as well as still-life or lifestyle imagery on location at restaurants, private members clubs, cafes or bars. The resulting photographs are then implemented across websites, social media and in print collateral such as magazines, cookbooks and various other press, marketing or advertising campaigns.',
+        img: 'https://i.ibb.co/YjLyTTk/521-5214421-hamburger-clip-art.png',
+        price: 500
+    },
+    {
+        id: 3,
+        title: 'Floral Photography',
+        shortDesc: 'lifestyle and studio photography for the floristry industry',
+        desc: 'Working closely with florists to create floral arrangements for e-commerce websites, social media and print collateral. Jamie Lau specialises in product photography with attention to detail that shows the exact colour and size of bouquets for online shops, to lifestyle photography with models and varied environments.',
+        img: 'https://i.ibb.co/ThnKQz6/7243580.png',
+        price: 300
+    },
+    {
+        id: 4,
+        title: 'Corporate Photography',
+        shortDesc: 'headshots, team portraits, workplaces, conferences and events',
+        desc: 'Corporate photography for amongst others, Marks & Spencer and Soho House, commissioned to take portraits of the people who make up these companies and visit workplaces to capture what makes working for these companies unique.',
+        img: 'https://i.ibb.co/Vq4JgWp/business-people-icon-png-10.png',
+        price: 300
+    },
+]
